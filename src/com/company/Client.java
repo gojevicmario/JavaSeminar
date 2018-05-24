1a3ee8a24461733e68b6f964d1629a946abb8d7b
gojevicmario <gojevic.mario@gmail.com>
2018-05-24 17:01:14 +0200

    Completed the task

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -6,18 +6,29 @@ import java.net.*;
 
 public class Client {
         public static void main(String[] args) throws IOException {
-            String sentence;
+            String sentence ;
             String modifiedSentence;
             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
 
-            Socket clientSocket = new Socket("localhost", 8080);
-            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            sentence = userInput.readLine();
-            outToServer.writeBytes(sentence + '\n');
-            modifiedSentence = inFromServer.readLine();
-            System.out.println(modifiedSentence);
-            clientSocket.close();
+            while(true){
+                Socket clientSocket = new Socket("localhost", 8080);
+                DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                sentence = userInput.readLine();
+                if(sentence.equalsIgnoreCase("end")){
+                    System.out.println("Thank you for using this amazing program");
+                    clientSocket.close();
+                    break;
+                }
+
+                outToServer.writeBytes(sentence + '\n');
+                modifiedSentence = inFromServer.readLine();
+                System.out.println(modifiedSentence);
+
+            }
+
+
         }
 }
